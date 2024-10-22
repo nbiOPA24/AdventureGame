f8bf1619edad45b174dc7abbb301df0a8530aaa7
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2024-10-22 16:10:06 +0200

    Added Racespecifik abilities to be used in combat later also created more folderstructure.

--- CharacterAndCombat/Combat/CombatHandler.cs
+++ CharacterAndCombat/Combat/CombatHandler.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
 
 public static  class CombatHandler 
 {
@@ -11,6 +12,7 @@ public static  class CombatHandler
     public static bool RunCombatScenario(List<Enemy> enemyList,Player player)
     {
         DisplayPlayerCombatOptions();
+        DisplayEnemyList(enemyList);
         
 
         //Checks if player is alive after the Combat has finnished
@@ -35,14 +37,26 @@ public static  class CombatHandler
         switch(Utilities.PickIndexFromList(combatOptions))
         {
             case 0:
+                //add method for choosing an ability from a list of active abilities
                 //attack code here choose ability  is probably the right name for it
                 break;
             case 1:
+                //TODO create items and inventory first
                 //Lists usable consumables such as healing potions or such
                 break;
             case 2:
                 //attempt to flee results in a loss
                 break;
+        }
+    }
+    public static void DisplayEnemyList(List<Enemy> enemyList)
+    {   
+        Console.WriteLine("Name           Health      Race");
+        foreach(Enemy e in enemyList)
+        {
+            Console.Write($"{e.Name,-15}");
+            Utilities.ConsoleWriteColor($"[{e.CurrentHealth}/{e.MaxHealth}]",ConsoleColor.DarkRed);
+            Console.WriteLine($"     {e.Race.RaceName}");
         }
     }
 }
---
+++ CharacterAndCombat/PlayableRaces/Human.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics.Contracts;
+
+public class Human : IRace
+{
+    #region Human Specific Abilities
+    Ability Slash = new Ability("Slash",10);
+    Ability DodgeRoll = new Ability("DodgeRoll",0);
+    #endregion
+    public string RaceName{get;set;}
+    public List<Ability> Abilities{get;set;}
+    public Human()
+    {
+        Abilities = new()
+        {
+            Slash,
+            DodgeRoll
+        };
+        RaceName = "Human";
+    }
+    public int  AdjustHealth(int baseHealth)
+    {
+        return baseHealth +2;
+    }
+    public int AdjustDamage(int baseDamage)
+    {
+        return baseDamage +2;
+    }
+    public List<Ability> GetAbilities()
+    {
+        return Abilities;
+    }
+    
+}
