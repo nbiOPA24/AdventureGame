abbd176e7ec068bb7833c42a7a4d46e5d59ab5db
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2024-10-24 10:21:52 +0200

    Added methods that return stringrepresentation of  Ability list/array

--- CharacterAndCombat/Abilities/Ability.cs
+++ CharacterAndCombat/Abilities/Ability.cs
@@ -8,4 +8,32 @@ public class Ability
         Name = name;
         BaseDamage = baseDamage;
     }
+
+
+    public static List<string> ToStringList(List<Ability> abilityList)
+    {
+        //puts all the names of the abilities into a string list
+            List<string> stringReturnList = new();
+            foreach(Ability a in abilityList)
+            {
+                stringReturnList.Add(a.Name);
+                Console.WriteLine(a.Name);
+            }
+            return stringReturnList;
+    }
+    public static List<string> ToStringList(Ability[] abilityList)
+    {
+        //puts all the names of the abilities into a string list
+            List<string> stringReturnList = new();
+            foreach(Ability a in abilityList)
+            {
+                if(a != null)
+                {
+                stringReturnList.Add(a.Name);
+                Console.WriteLine(a.Name);
+                }
+            }
+            return stringReturnList;
+    }
 }
+
