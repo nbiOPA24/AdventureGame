2877dad6026886efd3043d5094ffb48a5106145a
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2024-11-01 15:30:41 +0100

    Added methods to add cleanse and armorbuff to Abilities combateffect list.

--- CharacterAndCombat/Abilities/Ability.cs
+++ CharacterAndCombat/Abilities/Ability.cs
@@ -5,17 +5,19 @@ public class Ability
     public string Name {get;set;}
     public List<CombatEffect> CombatEffects {get;set;}
     public TargetType Target {get;set;}
+    public AbilityType Type {get;set;}
     public int CoolDownTimer {get;set;}
     public int CurrentCooldown {get;set;}
 
     
-    public Ability(string name,TargetType target,int cooldown)
+    public Ability(string name,TargetType target,int cooldown,AbilityType type)
     {
         Name = name;
         CombatEffects = new();
         Target = target;
         CoolDownTimer = cooldown+1;
         CurrentCooldown = cooldown+1;
+        Type = type;
 
     }
 
@@ -44,7 +46,20 @@ public class Ability
         Healing healing = new(magnitude);
         CombatEffects.Add(healing);
     }
-
+    public void AddArmorBuffEffect(int magnitude,int duration)
+    {
+        ArmorBuff armorbuff = new(duration,magnitude);
+        CombatEffects.Add(armorbuff);
+    }
+    public void AddCleanseEffect(List<eCombatEffect> effects)
+    {
+        Cleanse cleanse = new Cleanse(effects);
+        CombatEffects.Add(cleanse);
+    }
+    public Ability Clone()
+    {
+        return new Ability(Name,Target,CoolDownTimer-1,Type); 
+    }
 
 }
 
